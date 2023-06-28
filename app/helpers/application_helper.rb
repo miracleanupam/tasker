2946d2c1f04d240c2461c714d367b535cc095800
Anupam Dahal <miracleanupam@gmail.com>
2023-06-28 11:56:38 +0545

    page title bug fixed

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,23 @@
 module ApplicationHelper
   def get_task_status_class(status)
     case status
-    when "to_do"
-      "badge bg-secondary"
-    when "ongoing"
-      "badge bg-info"
-    when "completed"
-      "badge bg-success"
-    when "failed"
-      "badge bg-warning"
+    when 'to_do'
+      'badge bg-secondary'
+    when 'ongoing'
+      'badge bg-info'
+    when 'completed'
+      'badge bg-success'
+    when 'failed'
+      'badge bg-warning'
     else
-      "badge bg-danger"
+      'badge bg-danger'
     end
+  end
+
+  def get_full_page_title(page_title = '')
+    base_title = 'Tasker'
+    return base_title if page_title.empty?
+
+    "#{page_title} | #{base_title}"
   end
 end
